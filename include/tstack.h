44b551f3ee30c2a0435ea10f4e76019d8a1596e9
platna <160319937+platna@users.noreply.github.com>
2024-04-14 18:10:58 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,34 +1,34 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <string>
+
 template<typename T, int size>
 class TStack {
   // добавьте код стека
-};
  private:
-    T arr[100];
-    int top;
+    T *arr;
+    int tIndex;
+
  public:
-    TStack(): top(-1) {}
-    void push(T val) {
-      if (!isOverflow())
-        arr[++top] = val;
-      else
-        throw std::string("Stack overflow");
+    TStack() {
+        arr = new T[size];
+        tIndex = -1;
     }
-    T see() const { return arr[top]; }
-    T pop() {
-      if (isEmpty())
-        throw std::string("Stack is empty");
-      else
-        return arr[top--];
+    bool isEmpty() {
+        return tIndex == -1;
     }
-    bool isOverflow() const {
-      return top == size - 1;
+    void push(T value) {
+        arr[++tIndex] = value;
     }
-    bool isEmpty() const {
-      return top == -1;
+    T& pop() {
+        return arr[tIndex--];
+    }
+    bool isFull() const {
+        return tIndex == size;
+    }
+    T& get() const {
+        return arr[tIndex];
     }
 };
+
 #endif  // INCLUDE_TSTACK_H_
