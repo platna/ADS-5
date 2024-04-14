e1fb7cb0becd016d7e42bf36fd5db1fd07345b90
platna <160319937+platna@users.noreply.github.com>
2024-04-14 17:44:23 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -2,45 +2,35 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 #include <string>
-#include <stdexcept>
 
-template <typename T, int size>
+template<typename T, int size>
 class TStack {
-
-private:
+  // добавьте код стека
+};
  private:
-    T* data;
+    T arr[100];
     int top;
 
-public:
  public:
-    TStack() : top(-1) {
-        data = new T[size];
+    TStack(): top(-1) {}
+    void push(T val) {
+      if (!isOverflow())
+        arr[++top] = val;
+      else
+        throw std::string("Stack overflow");
     }
-    ~TStack() {
-        delete[] data;
-    }
-    void push(T value) {
-        if (!isFull()) {
-            data[++top] = value;
-        } else {
-            throw std::overflow_error("Stack is full");
-        }
-    }
-    T get() const {
-         return data[top];
-    }
+    T see() const { return arr[top]; }
     T pop() {
-        if (isEmpty()) {
-            throw std::underflow_error("Stack is empty");
-        }
-        return data[top--];
+      if (isEmpty())
+        throw std::string("Stack is empty");
+      else
+        return arr[top--];
+    }
+    bool isOverflow() const {
+      return top == size - 1;
     }
     bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-        return top == size - 1;
+      return top == -1;
     }
 };
-#endif // INCLUDE_TSTACK_H_
+#endif  // INCLUDE_TSTACK_H_
